0a92df6dce007c5f7a684e203e5635884b030c74
Douglas Seramim <douglasfseramim@gmail.com>
2025-02-24 20:05:33 -0300

    24/02

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,12 +6,16 @@ function App() {
   return (
     <>
       <h1>UNIPAR</h1>
-      <p>React com Vite</p>
+      <p id="52">React com Vite</p>
       <P/>
+      
+      <section >
+        <Card imagem = "https://i0.wp.com/blog.bioparquedorio.com.br/wp-content/uploads/2020/11/Abu_filhote_macaco-prego-do-peito-amarelo-BioParquedoRio.jpg?resize=1200%2C1200&ssl=1" 
+        titulo = "blablablablalba" descricao="1235432131"></Card>
+        <Card titulo = "ewidwudhuehuehuehu" descricao="7876872"></Card>
+        <Card titulo = "ihihiahduahuwhwaudhau"></Card>
+      </section>
 
-      <Card>Ablablabla</Card>
-      <Card>adwawdi</Card>
-      <Card>eueueueu</Card>
     </>
   )
 }
